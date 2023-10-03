6e7c80242b152570a392bc9c0c63e405ca83c629
alweb29 <albertow55@gmail.com>
2023-10-03 18:37:36 +0200

    Added loading screen while saving image

--- app/src/main/java/com/example/alweb29/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/alweb29/drawingapp/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.alweb29.drawingapp
 
 import android.Manifest
-import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Message
 import android.provider.MediaStore
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -19,11 +22,19 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView : DrawingView? = null
     private var mImageButtonCurrentPaint :ImageButton? = null
+    var customProgressDialog : Dialog? = null
 
     val openGalleryLauncher: ActivityResultLauncher<Intent> =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
@@ -92,6 +103,18 @@ class MainActivity : AppCompatActivity() {
             requestStoragePermission()
         }
 
+        val ibSave :ImageButton = findViewById(R.id.ib_save)
+        ibSave.setOnClickListener{
+
+            if (isReadStorageAllowed()){
+                showProgressDialog()
+                lifecycleScope.launch{
+                    val flDrawingView:FrameLayout = findViewById(R.id.fl_drawing_view_container)
+
+                    saveBitmapFile(getBitmapFromView(flDrawingView))
+                }
+            }
+        }
     }
 
     private fun showBrushSizeChooserDialog(){
@@ -138,6 +161,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun isReadStorageAllowed(): Boolean{
+        val result = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
     private fun requestStoragePermission(){
         if (ActivityCompat.shouldShowRequestPermissionRationale(
             this,
@@ -146,9 +176,72 @@ class MainActivity : AppCompatActivity() {
             showRationalDialog("Drawing App ", " Drawing App needs to Access Your External Storage")
         }else{
             requestPermission.launch(arrayOf(
-                Manifest.permission.READ_EXTERNAL_STORAGE
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
             ))
         }
+    }
+
+    private fun getBitmapFromView(view: View) : Bitmap{
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null){
+            bgDrawable.draw(canvas)
+        }else{
+            canvas.drawColor(Color.WHITE)
+        }
+
+        view.draw(canvas)
+        return returnedBitmap
+    }
+
+
+    private suspend fun saveBitmapFile(mBitmap: Bitmap?):String{
+        var result = ""
+        withContext(Dispatchers.IO){
+            if (mBitmap != null){
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+
+                    val f = File(externalCacheDir?.absoluteFile.toString() +
+                        File.separator.toString() + "DrawingApp_" + System.currentTimeMillis()/1000 + ".png"
+                    )
+                    
+                    val fo = FileOutputStream(f)
+                    fo.write((bytes.toByteArray()))
+                    fo.close()
+                    
+                    result = f.absolutePath
+                    
+                    runOnUiThread{
+
+                        cancelProgressDialog()
+
+                        if(result.isNotEmpty()){
+                            Toast.makeText(
+                                this@MainActivity,
+                                "File saved successfully : $result",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }else{
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Something went wrong",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                    
+                }
+                catch (e : Exception){
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return result
     }
 
     private fun showRationalDialog(
@@ -158,5 +251,20 @@ class MainActivity : AppCompatActivity() {
         val builder : AlertDialog.Builder = AlertDialog.Builder(this)
         builder.setTitle(title).setMessage(message).setPositiveButton("Cancel"){dialog, _ ->dialog.dismiss()}
         builder.create().show()
+    }
+
+    private fun showProgressDialog(){
+        customProgressDialog = Dialog(this@MainActivity)
+
+        customProgressDialog?.setContentView(R.layout.dialog_custom_progress)
+
+        customProgressDialog?.show()
+    }
+
+    private fun cancelProgressDialog(){
+        if (customProgressDialog!= null){
+            customProgressDialog?.dismiss()
+            customProgressDialog = null
+        }
     }
 }
